3dafe33f91a36a9ed35f780bec2998bd74f167f4
Ahmedhamdi-e7 <ahmedmashalyaa@gmail.com>
2021-01-03 09:01:11 +0200

    initial commit

---
+++ illustrios/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(illustrios.Startup))]
+namespace illustrios
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
